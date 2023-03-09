45050c6df16fac2b5ba066e55c2098a70a31b2ab
Luis Florez <braian1k3@gmail.com>
2023-03-09 11:39:53 -0500

    Corregir proyecto y crear archivo readme.txt para responder preguntas.

--- src/main/java/org/example/Restaurante.java
+++ src/main/java/org/example/Restaurante.java
@@ -1,31 +1,32 @@
 package org.example;
 
-public class Restaurante {
+import java.text.MessageFormat;
+
+public class
+Restaurante {
     public static void main(String[] args) {
 
         //voy con mi esposa a cenar a un restaurante.
         //Cada uno pide una cena:
 
-        Comida comidaUno= new Comida()
-        comidaUno.entrada= new Entrada("Sopa de esparragos",28000);
-        comidaUno.platofuerte= new PlatoFuerte("beef chorizo",55000);
-        comidaUno.bebida=new Bebida("Vino tinto" 45000);
-        comidaUno.postre= new Postre("tiramizu escarchado",34500);
-        Integer costoComida1=comidaUno.calcularCostoComida();
+        Comida comidaUno= new Comida();
+        comidaUno.setEntrada(new Entrada("Sopa de esparragos",28000));
+        comidaUno.setPlatofuerte(new PlatoFuerte("beef chorizo",55000));
+        comidaUno.setBebida(new Bebida("Vino tinto", 45000));
+        comidaUno.setPostre(new Postre("tiramizu escarchado",34500));
 
-
-
+        Integer costoComida1 = comidaUno.calcularCostoComida();
 
         Comida comidaDos = new Comida();
-        comidaDos.entrada= new Entrada("muelas de cangrejo",30000);
-        comidaDos.platofuerte= new PlatoFuerte("pato a la naranja" 58000);
-        comidaDos.bebida=new Bebida("Soda de miel y tamarindo",18500);
-        comidaDos.postre= new Postre("volcan de ciruelas",22000);
-        Integer costoComida2=comidaDos.calcularCostoComida();
+        comidaDos.setEntrada(new Entrada("muelas de cangrejo",30000));
+        comidaDos.setPlatofuerte(new PlatoFuerte("pato a la naranja", 58000));
+        comidaDos.setBebida(new Bebida("Soda de miel y tamarindo",18500));
+        comidaDos.setPostre(new  Postre("volcan de ciruelas",22000));
 
+        Integer costoComida2 = comidaDos.calcularCostoComida();
 
-        System.out.println("El costo total de la cena para dos fue de: ");
+        System.out.print("El costo total de la cena para dos fue de: $");
         //PONGA ACA LA LINEA QUE PERMITA ENCONTRAR EL REUSLTADO
-
+        System.out.println(costoComida1 + costoComida2);
     }
 }
